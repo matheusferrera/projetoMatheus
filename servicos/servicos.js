b36c18f8405d7bbbc6559e80c25a7e39639dc687
Matheus Ferreira <mat.matheusf@hotmail.com>
2023-06-12 14:34:27 -0300

    push 2.0

--- servicos/servicos.js
+++ servicos/servicos.js
@@ -1,3 +1,15 @@
+// Armazena as referências aos elementos para evitar acessos repetidos ao DOM
+const circleSvgServicos = document.querySelector("#circleSvg-servicos");
+const maskDivServicos = document.querySelector("#maskDiv-servicos");
+const servico1 = document.querySelector("#servico1");
+const servico2 = document.querySelector("#servico2");
+const servico3 = document.querySelector("#servico3");
+const servico1Text = document.querySelector("#servico1-text");
+const servico2Text = document.querySelector("#servico2-text");
+const servico3Text = document.querySelector("#servico3-text");
+
+
+
 function animacaoServ(){
 
     let scroll = this.scrollY;
@@ -6,64 +18,64 @@ function animacaoServ(){
     {
         scroll = scroll - 2000 
         
-        document.getElementById("circleSvg-servicos").setAttribute("cx", (50 + (scroll/3)) + "vw"); 
-        document.getElementById("circleSvg-servicos").setAttribute("cy", (50 + (scroll/3)) + "vw"); 
-        document.getElementById("circleSvg-servicos").setAttribute("r", (40 + (scroll/4)) + "vw"); 
+        circleSvgServicos.setAttribute("cx", (50 + (scroll/3)) + "vw"); 
+        circleSvgServicos.setAttribute("cy", (50 + (scroll/3)) + "vw"); 
+        circleSvgServicos.setAttribute("r", (40 + (scroll/4)) + "vw"); 
         
-        document.getElementById("maskDiv-servicos").style.left = (0 - (scroll/3)) + "vw";
-        document.getElementById("maskDiv-servicos").style.top = (85 - (scroll/5)) + "vh";
+        maskDivServicos.style.left = (0 - (scroll/3)) + "vw";
+        maskDivServicos.style.top = (85 - (scroll/5)) + "vh";
 
     }
 
     if(this.scrollY < 2000){
-        document.getElementById("circleSvg-servicos").setAttribute("cx", "50vw"); 
-        document.getElementById("circleSvg-servicos").setAttribute("cy", "50vw"); 
-        document.getElementById("circleSvg-servicos").setAttribute("r",  "40vw"); 
+        circleSvgServicos.setAttribute("cx", "50vw"); 
+        circleSvgServicos.setAttribute("cy", "50vw"); 
+        circleSvgServicos.setAttribute("r",  "40vw"); 
         
-        document.getElementById("maskDiv-servicos").style.left = "0vw";
-        document.getElementById("maskDiv-servicos").style.top = "85vh";
+        maskDivServicos.style.left = "0vw";
+        maskDivServicos.style.top = "85vh";
     }
 
     if (this.scrollY > 2000 && this.scrollY < 3000){
-        document.getElementById("servico1").style.color = "#97FF66";
-        document.getElementById("servico1").style.fontSize = "2rem";
-        document.getElementById("servico1-text").style.opacity = "1";
+        servico1.style.color = "#97FF66";
+        servico1.style.fontSize = "2rem";
+        servico1Text.style.opacity = "1";
 
-        document.getElementById("servico2").style.color = "grey";
-        document.getElementById("servico2").style.fontSize = "1.5rem";
-        document.getElementById("servico2-text").style.opacity = "0";
+        servico2.style.color = "grey";
+        servico2.style.fontSize = "1.5rem";
+        servico2Text.style.opacity = "0";
 
-        document.getElementById("servico3").style.color = "grey";
-        document.getElementById("servico3").style.fontSize = "1.5rem";
-        document.getElementById("servico3-text").style.opacity = "0";
+        servico3.style.color = "grey";
+        servico3.style.fontSize = "1.5rem";
+        servico3Text.style.opacity = "0";
     }
 
     if (this.scrollY > 3000 && this.scrollY < 3300){
-        document.getElementById("servico1").style.color = "grey";
-        document.getElementById("servico1").style.fontSize = "1.5rem";
-        document.getElementById("servico1-text").style.opacity = "0";
+        servico1.style.color = "grey";
+        servico1.style.fontSize = "1.5rem";
+        servico1Text.style.opacity = "0";
 
-        document.getElementById("servico2").style.color = "#97FF66";
-        document.getElementById("servico2").style.fontSize = "2rem";
-        document.getElementById("servico2-text").style.opacity = "1";
+        servico2.style.color = "#97FF66";
+        servico2.style.fontSize = "2rem";
+        servico2Text.style.opacity = "1";
 
-        document.getElementById("servico3").style.color = "grey";
-        document.getElementById("servico3").style.fontSize = "1.5rem";
-        document.getElementById("servico3-text").style.opacity = "0";
+        servico3.style.color = "grey";
+        servico3.style.fontSize = "1.5rem";
+        servico3Text.style.opacity = "0";
     }
 
     if (this.scrollY > 3300 && this.scrollY < 3600){
-        document.getElementById("servico1").style.color = "grey";
-        document.getElementById("servico1").style.fontSize = "1.5rem";
-        document.getElementById("servico1-text").style.opacity = "0";
+        servico1.style.color = "grey";
+        servico1.style.fontSize = "1.5rem";
+        servico1Text.style.opacity = "0";
 
-        document.getElementById("servico2").style.color = "grey";
-        document.getElementById("servico2").style.fontSize = "1.5rem";
-        document.getElementById("servico2-text").style.opacity = "0";
+        servico2.style.color = "grey";
+        servico2.style.fontSize = "1.5rem";
+        servico2Text.style.opacity = "0";
 
-        document.getElementById("servico3").style.color = "#97FF66";
-        document.getElementById("servico3").style.fontSize = "2rem";
-        document.getElementById("servico3-text").style.opacity = "1";
+        servico3.style.color = "#97FF66";
+        servico3.style.fontSize = "2rem";
+        servico3Text.style.opacity = "1";
     }
 
     requestAnimationFrame(animacaoServ);
