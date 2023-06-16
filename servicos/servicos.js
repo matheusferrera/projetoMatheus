ed209853386088bd74e36140677fa56f21d9ccd6
Matheus Ferreira <mat.matheusf@hotmail.com>
2023-06-16 10:05:17 -0300

    teste firefox

--- servicos/servicos.js
+++ servicos/servicos.js
@@ -1,12 +1,12 @@
 // Armazena as referências aos elementos para evitar acessos repetidos ao DOM
-const circleSvgServicos = document.querySelector("#circleSvg-servicos");
-const maskDivServicos = document.querySelector("#maskDiv-servicos");
-const servico1 = document.querySelector("#servico1");
-const servico2 = document.querySelector("#servico2");
-const servico3 = document.querySelector("#servico3");
-const servico1Text = document.querySelector("#servico1-text");
-const servico2Text = document.querySelector("#servico2-text");
-const servico3Text = document.querySelector("#servico3-text");
+const circleSvgServicos = document.getElementById("circleSvg-servicos");
+const maskDivServicos = document.getElementById("maskDiv-servicos");
+const servico1 = document.getElementById("servico1");
+const servico2 = document.getElementById("servico2");
+const servico3 = document.getElementById("servico3");
+const servico1Text = document.getElementById("servico1-text");
+const servico2Text = document.getElementById("servico2-text");
+const servico3Text = document.getElementById("servico3-text");
 
 // Define as classes CSS para aplicar as alterações de estilo
 const activeServicoClass = "active-servico";
@@ -21,9 +21,12 @@ function animacaoServ() {
   // Verifica se o scroll mudou significativamente antes de realizar as atualizações
   if (Math.abs(scroll - lastScroll) > 10) {
     
+    //CASO MOBILE
+    if(window.innerWidth < 600){
 
-    // Animação para mobile
-    if (window.scrollY > 2000 && window.innerHeight < 1000) {
+    // Atualiza a posição do círculo SVG
+    console.log("entrou")
+    if (window.scrollY > 2000) {
       const circleScroll = scroll - 2000;
       const circleCx = 50 + circleScroll / 3 + "vw";
       const circleCy = 50 + circleScroll / 3 + "vw";
@@ -38,41 +41,13 @@ function animacaoServ() {
       maskDivServicos.style.top = maskTop;
     } 
     
-    if(window.scrollY < 2000 && window.innerHeight < 1000) {
+    if(window.scrollY < 2000) {
       circleSvgServicos.setAttribute("cx", "50vw");
       circleSvgServicos.setAttribute("cy", "50vw");
       circleSvgServicos.setAttribute("r", "40vw");
 
       maskDivServicos.style.left = "0vw";
       maskDivServicos.style.top = "85vh";
-    }
-
-
-
-    // Animação para desktop
-    if (window.scrollY > 1000 && window.innerHeight > 1000) {
-      const circleScroll = scroll - 1000;
-      const circleCx = 50 + circleScroll / 3 + "vw";
-      const circleCy = 50 + circleScroll / 3 + "vw";
-      const circleR = 40 + circleScroll / 4 + "vw";
-      circleSvgServicos.setAttribute("cx", circleCx);
-      circleSvgServicos.setAttribute("cy", circleCy);
-      circleSvgServicos.setAttribute("r", circleR);
-
-      const maskLeft = 0 - (scroll - 1000) / 3 + "vw";
-      const maskTop = 75 - (scroll - 1000) / 5 + "vh";
-      maskDivServicos.style.left = maskLeft;
-      maskDivServicos.style.top = maskTop;
-    } 
-
-    if(window.scrollY < 1000 && window.innerHeight > 1000) {
-      console.log("ESTAMOS NO DESKTOP ---> " + window.innerHeight)
-      circleSvgServicos.setAttribute("cx", "50vw");
-      circleSvgServicos.setAttribute("cy", "50vw");
-      circleSvgServicos.setAttribute("r", "40vw");
-
-      maskDivServicos.style.left = "0vw";
-      maskDivServicos.style.top = "75vh";
     }
 
     // Atualiza o estado dos serviços
@@ -119,6 +94,90 @@ function animacaoServ() {
 
     // Atualiza o último valor de scroll
     lastScroll = scroll;
+    }
+
+    
+
+
+    //Caso PC
+    else if(window.innerWidth >= 600){
+    // Atualiza a posição do círculo SVG
+    console.log(window.scrollY)
+    if (window.scrollY > 500) {
+      const circleScroll = 2*(scroll - 500);
+      const circleCx = 50 + (circleScroll / 3) + "vw";
+      const circleCy = 50 + (circleScroll / 3) + "vw";
+      const circleR = 40 + (circleScroll / 4) + "vw";
+      circleSvgServicos.setAttribute("cx", circleCx);
+      circleSvgServicos.setAttribute("cy", circleCy);
+      circleSvgServicos.setAttribute("r", circleR);
+    
+      
+      const maskLeft = 0 - (circleScroll / 3) + "vw";
+      const maskTop = 70 - (circleScroll / 4) + "vh";
+  
+      maskDivServicos.style.left = maskLeft;
+      maskDivServicos.style.top = maskTop;
+
+      console.log(circleCx + " ---- " + maskLeft)
+    } 
+    
+    if(window.scrollY < 500) {
+      circleSvgServicos.setAttribute("cx", "50vw");
+      circleSvgServicos.setAttribute("cy", "50vw");
+      circleSvgServicos.setAttribute("r", "40vw");
+
+      maskDivServicos.style.left = "0vw";
+      maskDivServicos.style.top = "70vh";
+    }
+
+    // Atualiza o estado dos serviços
+    if (window.scrollY > 2000 && window.scrollY < 3000) {
+      servico1.classList.add(activeServicoClass);
+      servico1.style.fontSize = "2rem";
+      servico1Text.classList.add(activeServicoTextClass);
+
+      servico2.classList.remove(activeServicoClass);
+      servico2.style.fontSize = "1.5rem";
+      servico2Text.classList.remove(activeServicoTextClass);
+
+      servico3.classList.remove(activeServicoClass);
+      servico3.style.fontSize = "1.5rem";
+      servico3Text.classList.remove(activeServicoTextClass);
+    } 
+    
+    if (window.scrollY > 3000 && window.scrollY < 3300) {
+      servico1.classList.remove(activeServicoClass);
+      servico1.style.fontSize = "1.5rem";
+      servico1Text.classList.remove(activeServicoTextClass);
+
+      servico2.classList.add(activeServicoClass);
+      servico2.style.fontSize = "2rem";
+      servico2Text.classList.add(activeServicoTextClass);
+
+      servico3.classList.remove(activeServicoClass);
+      servico3.style.fontSize = "1.5rem";
+      servico3Text.classList.remove(activeServicoTextClass);
+    } 
+    if (window.scrollY > 3300 && window.scrollY < 3600) {
+      servico1.classList.remove(activeServicoClass);
+      servico1.style.fontSize = "1.5rem";
+      servico1Text.classList.remove(activeServicoTextClass);
+
+      servico2.classList.remove(activeServicoClass);
+      servico2.style.fontSize = "1.5rem";
+      servico2Text.classList.remove(activeServicoTextClass);
+
+      servico3.classList.add(activeServicoClass);
+      servico3.style.fontSize = "2rem";
+      servico3Text.classList.add(activeServicoTextClass);
+    }
+
+    // Atualiza o último valor de scroll
+    lastScroll = scroll;
+    }
+
+   
   }
 
   requestAnimationFrame(animacaoServ);
